34ba1002fd55a33d8a3bd96a1554438ab658a850
Bosseyang <bosse_yang@hotmail.com>
2025-08-29 14:59:17 +0200

    Update RegistrationForm.tsx
    all the required inputs added and wrapped for layout

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -20,6 +20,20 @@ export const RegistrationForm: React.FC = () => {
     <form className="form" onSubmit={handleSubmit}>
       <h2 className="form-h2">Create an Account</h2>
       <InputField label="Name" type="text" value={name} onChange={setName} />
+      <div className="input-wrapper">
+        <div className="user-wrapper">
+          <InputField label="Name" type="text" value={name} onChange={setName} />
+          <InputField label="Username" type="text" value={username} onChange={setUsername} />
+          <InputField label="Email" type="email" value={email} onChange={setEmail} />
+        </div>
+        <div className="password-wrapper">
+          <InputField label="Password" type="password" value={password} onChange={setPassword} />
+          <InputField
+            label="Confirm Password"
+            type="password"
+            value={confirmPassword}
+            onChange={setConfirmPassword}
+          />
 
       <button type="submit">Submit</button>
           <PasswordValidation isPasswordValid={isPasswordValid} passwordsMatch={passwordsMatch} />

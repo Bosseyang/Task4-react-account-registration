92c024ef9eb64465877cd28358893c32dd11c78d
Bosseyang <bosse_yang@hotmail.com>
2025-08-29 12:09:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Task4-react-account-registration
+Lexicon IT Education | Frontend | React TS | Task 4 | React Account Registration
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export const Header = (): ReactElement => {
+  return (
+    <header className="header">
+      <h1 className="header-h1">Account Registration</h1>
+    </header>
+  );
+};

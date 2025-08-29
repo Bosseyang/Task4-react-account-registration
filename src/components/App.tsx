56305373fa5f95f297a25fa05954ded9c3fc2cde
Bosseyang <bosse_yang@hotmail.com>
2025-08-29 13:25:30 +0200

    Update App.tsx
    implement RegistrationForm

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
 import "../css/App.css";
 import { Header } from "./Header";
+import { RegistrationForm } from "./RegistrationForm";
 
 function App() {
   return (
     <body>
       <Header />
+      <main>
+        <RegistrationForm />
+      </main>
     </body>
   );
 }

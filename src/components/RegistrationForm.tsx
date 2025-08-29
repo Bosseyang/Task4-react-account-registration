903a55dab972acb1b7aca9988b5a0c75774ef1ac
Bosseyang <bosse_yang@hotmail.com>
2025-08-29 13:26:22 +0200

    Update RegistrationForm.tsx
    add inputs, add button placeholder, needs to be update to its own component

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -3,13 +3,25 @@ import { InputField } from "./InputField";
 
 export const RegistrationForm: React.FC = () => {
   const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const isPasswordValid = password.length >= 0;
+  const passwordsMatch = password === confirmPassword;
+  const canSubmit = isPasswordValid && passwordsMatch;
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!canSubmit) return;
   };
   return (
     <form className="form" onSubmit={handleSubmit}>
       <h2 className="form-h2">Create an Account</h2>
       <InputField label="Name" type="text" value={name} onChange={setName} />
+
+      <button type="submit">Submit</button>
     </form>
   );
 };
